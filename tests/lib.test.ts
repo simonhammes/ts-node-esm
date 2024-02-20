4183415daee02ea33bf736396c2464a8c704ab0c
Simon <10352679+simonhammes@users.noreply.github.com>
2024-02-21 00:35:23 +0100

    Node 21 (#53)

--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -1,18 +1,20 @@
+import assert from 'node:assert';
+import test from 'node:test';
 import { foo, numbers } from '../src/lib.js';
 
 test('1 + 2', () => {
-    expect(1 + 2).toBe(3);
+    assert.strictEqual(1 + 2, 3);
 });
 
 test('foo()', () => {
-   expect(foo()).toBe('Hello World!');
+    assert.strictEqual(foo(), 'Hello World!');
 });
 
 test('numbers', () => {
-    expect(numbers.length).toBeGreaterThan(2);
+    assert(numbers.length > 2);
 });
 
 test('TypeScript works', () => {
     const fn = (text: string): string => text;
-    expect(fn('Hello World!')).toBe('Hello World!');
+    assert.strictEqual(fn('Hello World!'), 'Hello World!');
 });
